d0bd763cef0c9e6a23a5d3271ecbc7290eae385c
chen-456 <4149836+chen-456@users.noreply.github.com>
2023-03-29 22:23:30 +0800

    [web] Add `FileListView`

---
+++ frontend/src/views/FileListView.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { ref, type Ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+import { ensureConnection } from '@/utils/control';
+import { useUserStore } from '@/stores/user';
+
+const router = useRouter();
+const userStore = useUserStore();
+
+interface DirEntry {
+  name: string;
+  directory: boolean;
+  size: number | null;
+}
+
+const props = defineProps<{
+  path: string,
+}>();
+const curListing: Ref<DirEntry[] | undefined> = ref();
+
+function iconOf(dirEntry: DirEntry): string {
+  return dirEntry.directory ? 'mdi-folder' : 'mdi-file';
+}
+
+async function fetchDirEntries() {
+  if (userStore.current === undefined) {
+    curListing.value = undefined;
+    return;
+  }
+
+  try {
+    const controlSocket = await ensureConnection();
+    const result = await controlSocket.execute({
+      'cmd': 'ListDir',
+      'path': props.path,
+    });
+    if (result.err !== null) {
+      alert('Failed to list directory: ' + result.err);
+    } else {
+      curListing.value = result.entries;
+    }
+  } catch (e) {
+    alert('Failed to list directory: ' + e);
+  }
+}
+
+function onNavigate(entry: DirEntry) {
+  // TODO: support file downloading
+  if (!entry.directory)
+    return;
+
+  let newPath = props.path;
+  if (newPath.length !== 0)
+    newPath += '/';
+  newPath += entry.name;
+  router.push('/?path=' + encodeURIComponent(newPath));
+}
+
+fetchDirEntries();
+userStore.$subscribe(() => fetchDirEntries());
+watch(() => [props.path], () => fetchDirEntries());
+</script>
+
+<template>
+  <v-list v-if="curListing !== undefined">
+    <v-list-item v-for="(entry, i) in curListing" :key="i" :value="entry" active-color="primary" variant="plain"
+      @click="onNavigate(entry)">
+      <template v-slot:prepend>
+        <v-icon :icon="iconOf(entry)" />
+      </template>
+
+      <v-list-item-title>{{ entry.name }}</v-list-item-title>
+    </v-list-item>
+  </v-list>
+
+  <v-container v-if="curListing === undefined">
+    <!-- Loading animation -->
+    <template v-if="userStore.current !== undefined">
+      <v-progress-circular indeterminate />
+      &nbsp;Loading entries...
+    </template>
+
+    <!-- Log in prompt -->
+    <template v-if="userStore.current === undefined">
+      <v-icon icon="mdi-alert-outline" />
+      &nbsp;Not logged in
+    </template>
+  </v-container>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,35 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import FileListView from './FileListView.vue';
+
+const route = useRoute();
+
+const currentPath = computed(() => (route.query.path as string) || '');
+const breadcrumbDisplay = computed(() => {
+  const result = [{ 'title': '/', 'to': '/' }];
+
+  const segments = currentPath.value.split('/');
+  if (segments.length === 0)
+    return result;
+
+  let curPath = '';
+  for (const [i, segment] of segments.entries()) {
+    if (i !== 0)
+      curPath += '/';
+    curPath += segment;
+    result.push({ 'title': segment, 'to': '/?path=' + encodeURIComponent(curPath) });
+  }
+  return result;
+});
 </script>
 
 <template>
-  Home
+  <v-breadcrumbs :items="breadcrumbDisplay">
+    <template v-slot:divider>
+      <v-icon icon="mdi-chevron-right" />
+    </template>
+  </v-breadcrumbs>
+
+  <file-list-view :path="currentPath" />
 </template>
