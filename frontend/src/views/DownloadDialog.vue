09e6ab8e915726c5b822b95bf0fe899a20b57a54
chen-456 <4149836+chen-456@users.noreply.github.com>
2023-03-30 17:09:39 +0800

    [web] Fully implement download logic

--- frontend/src/views/DownloadDialog.vue
+++ frontend/src/views/DownloadDialog.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ensureConnection } from '@/utils/control';
+import { computed, ref, type Ref } from 'vue';
 
 const props = defineProps<{
   path: string,
@@ -8,26 +9,66 @@ const emit = defineEmits<{
   (event: 'finish'): void,
 }>();
 const visible = ref(true);
+const uuid: Ref<string | undefined> = ref(), error: Ref<string | undefined> = ref();
+const downloadLink = computed(() => 'http://localhost:8080/api/file/' + uuid.value);
 
 function finish() {
+  // Control commands cannot be terminated, so we just ignore its results
   visible.value = false;
   setTimeout(() => emit('finish'), 1000);
 }
 
-console.log('Download', props.path);
+async function fetchDownloadLink() {
+  try {
+    const controlSocket = await ensureConnection();
+    const result = await controlSocket.execute({
+      'cmd': 'Download',
+      'path': props.path,
+    });
+    if (result.err === null) {
+      uuid.value = result.uuid;
+      window.open(downloadLink.value);
+    } else {
+      error.value = result.err;
+    }
+  } catch (e: any) {
+    console.error('Failed to fetch download link:', e);
+    error.value = e.toString();
+  }
+}
+
+// console.log('Download', props.path);
+fetchDownloadLink();
 </script>
 
 <template>
   <v-dialog v-model="visible" width="400px">
     <v-card width="100%" title="Download file">
-      <template #text>
+      <!-- Download link-->
+      <v-card-text v-if="uuid !== undefined">
+        If the download does not start automatically, click
+        <a :href="downloadLink">here</a>.
+      </v-card-text>
+
+      <!-- Progress -->
+      <v-card-text v-if="uuid === undefined && error === undefined">
         <v-progress-circular indeterminate color="primary" />
         &nbsp;Fetching download link...
-      </template>
-      <template #actions>
+      </v-card-text>
+
+      <!-- Error -->
+      <v-card-text v-if="uuid === undefined && error !== undefined">
+        <v-progress-circular indeterminate color="primary" />
+        &nbsp;Failed to fetch download link: {{ error }}
+      </v-card-text>
+
+      <!-- Actions -->
+      <v-card-actions>
         <v-spacer />
-        <v-btn color="red-darken-2" @click="finish">Cancel</v-btn>
-      </template>
+        <v-btn color="red-darken-2" @click="finish">
+          {{ (uuid === undefined && error === undefined) ? 'Cancel' : 'Dismiss' }}
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
