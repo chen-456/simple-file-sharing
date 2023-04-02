684e9f0bdb9fb685d4e1d8f3e9a3730e68ab96a5
chen-456 <4149836+chen-456@users.noreply.github.com>
2023-04-02 14:30:14 +0800

    [web] `LoginDialog`: Add Register button

--- frontend/src/views/LoginDialog.vue
+++ frontend/src/views/LoginDialog.vue
@@ -15,7 +15,7 @@ function required(v: string) {
   return !!v || 'Field is required';
 }
 
-async function doLogin() {
+async function doLoginOrRegister(register: boolean) {
   if (username.value.length === 0 || password.value.length === 0) {
     alert('Missing required fields');
     return;
@@ -25,7 +25,7 @@ async function doLogin() {
   try {
     const controlSocket = await ensureConnection();
     await controlSocket.execute({
-      'cmd': 'LoginPwd',
+      'cmd': register ? 'RegisterPwd' : 'LoginPwd',
       'username': username.value,
       'password': password.value,
     });
@@ -51,13 +51,16 @@ defineExpose({ show });
       <v-card-title>
         <span class="text-h5">Log in</span>
       </v-card-title>
-      <v-form @submit.prevent="doLogin">
+      <v-form @submit.prevent="void (0)">
         <v-text-field v-model="username" label="Username" :rules="[required]" clearable :readonly="busy" />
         <v-text-field v-model="password" label="Password" type="password" :rules="[required]" clearable
           :readonly="busy" />
         <br />
         <v-btn type="submit" block size="large" color="success" variant="elevated" :disabled="busy"
-          @click="doLogin()">Login</v-btn>
+          @click="doLoginOrRegister(false)">Login</v-btn>
+        <br />
+        <v-btn type="submit" block size="large" color="blue" variant="elevated" :disabled="busy"
+          @click="doLoginOrRegister(true)">Register</v-btn>
         <br />
         <v-btn color="failed" block size="large" variant="elevated" @click="visible = false">Cancel</v-btn>
       </v-form>
